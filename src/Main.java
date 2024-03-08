01cedf03733e5083ad3cdf09b2863631df0fabfb
Techmaster3000 <52240882+Techmaster3000@users.noreply.github.com>
2024-03-08 14:00:19 +0100

    Added a spaceInvader Ascii function

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void SpaceInvader() {
+        System.out.println("                    ############");
+        System.out.println("                ####################");
+        System.out.println("              ########################");
+        System.out.println("            ####  ####  ####  ####  ####");
+        System.out.println("          ################################");
+        System.out.println("              ######    ####    ######");
+        System.out.println("                ##                ##");
+
+
+    }
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        SpaceInvader();
         System.out.println("This is the start of RetroReviewer!");
     }
 }
